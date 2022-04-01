fd4dce085ef72354a5eea0b58b40a932165621ac
victor <victorkrook96@gmail.com>
2022-04-01 16:19:10 +0200

    Added debug prints

--- main/src/platform.c
+++ main/src/platform.c
@@ -1,19 +1,26 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "esp_log.h"
+
 #include "platform.h"
 #include "i2c.h"
 #include "slamdeck.h"
 
 static const char* TAG = "Platform";
 
+//#define DO_DEBUG
 
 uint8_t RdByte(
 		VL53L5CX_Platform *p_platform,
 		uint16_t RegisterAdress,
 		uint8_t *p_value)
 {
-	return i2c_master_read_from_reg16(p_platform->address, RegisterAdress, p_value, 1);
+	uint8_t r = i2c_master_read_from_reg16(p_platform->address, RegisterAdress, p_value, 1);
+	#ifdef DO_DEBUG
+		ESP_LOGD(TAG, "[%02x] Read %02x to %04x", p_platform->address, *p_value, RegisterAdress);
+	#endif
+	return r;
 }
 
 uint8_t WrByte(
@@ -21,7 +28,11 @@ uint8_t WrByte(
 		uint16_t RegisterAdress,
 		uint8_t value)
 {
-	return i2c_master_write_to_reg16(p_platform->address, RegisterAdress, &value, 1);
+	#ifdef DO_DEBUG
+		ESP_LOGD(TAG, "[%02x] Writing %02x to %04x", p_platform->address, value, RegisterAdress);
+	#endif
+	uint8_t r = i2c_master_write_to_reg16(p_platform->address, RegisterAdress, &value, 1);
+	return r;
 }
 
 uint8_t WrMulti(
